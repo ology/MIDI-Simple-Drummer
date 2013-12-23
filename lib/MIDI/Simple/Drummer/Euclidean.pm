14898655662a1167b13f58ebc170c4d221748dba
Gene Boggs <gene.boggs@gmail.com>
2013-12-22 23:43:27 -0800

    WIP: "...but a stub."

--- lib/MIDI/Simple/Drummer/Euclidean.pm
+++ lib/MIDI/Simple/Drummer/Euclidean.pm
@@ -1,22 +1,45 @@
 package MIDI::Simple::Drummer::Euclidean;
-our $VERSION = '0.0100';
+our $VERSION = '0.00_01';
 use strict;
 use warnings;
 use base 'MIDI::Simple::Drummer';
 
-sub _setup {
+sub new {
     my $self = shift;
-    $self->SUPER::_setup(@_);
+    $self->SUPER::new(
+        -patch  => 25,
+        -tr_808 => 0,
+        @_
+    );
+    # Use the requested kit.
+    if ($self->{-tr_808}) {
+        $self->patch(26);
+    }
 }
 
 sub _default_patterns {
     my $self = shift;
     return {
 
-1 => sub { my $self = shift; }, # TBD...
+1 => \&euclid,
 
     };
 }
+
+sub euclid {
+    my $self = shift;
+    my ($m, $n) = @_;
+
+    my $ones = 1 x $m;
+    my $zeros = 0 x ($n - $m);
+    
+    if (@k <= 1) {
+        return $m;
+    }
+    else {
+        return euclid($n % $m, $m);
+    }
+};
 
 1;
 __END__
@@ -27,21 +50,18 @@ MIDI::Simple::Drummer::Euclidean - Euclidean Rhythms
 
 =head1 DESCRIPTION
 
-This package contains a collection of Euclidean based patterns that are loaded
-by L<MIDI::Simple::Drummer>.
-
-Additionally, the methods below are available.
-
-=head1 METHODS
-
-=head2 foo()
-
-Foo!
+Sadly, this module is but a stub.
 
 =head1 SEE ALSO
 
 L<MIDI::Simple::Drummer>, the F<eg/euclidean> and F<t/05-Euclidean.t> test
 scripts.
+
+L<http://student.ulb.ac.be/~ptaslaki/publications/phdThesis-Perouz.pdf>
+
+L<http://cgm.cs.mcgill.ca/~godfried/publications/banff.pdf>
+
+L<http://www.ageofthewheel.com/2011/03/euclidean-rhythm-midi-file-resource-in.html>
 
 =cut
 
